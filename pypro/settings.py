fca70cfcdd11582bd27e694e7bb63aac865fbe9f
Enos Teteo <enosfrancisco@gmail.com>
2020-07-13 22:49:07 -0300

    arquivos para deploy no heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-njk@g)5(&$a5qea#%cox&0hvik25trt!7gprfntyc5t*g)jdh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
